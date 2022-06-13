b377aa04db3e90cbc21a3b0d6311fc971a6d58e5
xjh <934622645@qq.com>
2022-06-13 08:04:39 +0800

    init project

---
+++ src/main/java/org/xjh/freemovieserver/FreeMovieServerApplication.java
@@ -0,0 +1,13 @@
+package org.xjh.freemovieserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeMovieServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeMovieServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/xjh/freemovieserver/FreeMovieServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xjh.freemovieserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeMovieServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

69c2ea537bc083a46a4ac7262b278f2ca80f7b51
xjh <934622645@qq.com>
2022-06-18 17:35:44 +0800

    framework changed Quarkus

---
+++ src/main/java/org/xjh/movie/GreetingConfig.java
@@ -0,0 +1,12 @@
+package org.xjh.movie;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
